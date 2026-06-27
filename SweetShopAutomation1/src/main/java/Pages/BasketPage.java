b0514e2d05c910e39120abd3d80d5c8e0bf46799
yasaebrahem1-ops <yasaebrahem1@gmail.com>
2026-06-27 15:30:42 +0300

    Add SweetShopAutomation1 folder

---
+++ SweetShopAutomation1/src/main/java/Pages/BasketPage.java
@@ -0,0 +1,53 @@
+package Pages;
+
+import Utilities.Utility;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class BasketPage {
+
+    final WebDriver driver;
+
+    By firstNameInput = By.xpath("//label[contains(text(), 'First name')]/following-sibling::input");
+    By lastNameInput = By.xpath("//label[contains(text(), 'Last name')]/following-sibling::input");
+    By emailInput = By.xpath("//label[contains(text(), 'Email')]/following-sibling::input");
+    By addressInput = By.id("address");
+    By address2Input = By.id("address2");
+    By countryDropdown = By.id("country");
+    By cityDropdown = By.id("city");
+    By zipInput = By.id("zip");
+
+    By nameOnCardInput = By.id("cc-name");
+    By creditCardNumberInput = By.id("cc-number");
+    By expirationInput = By.id("cc-expiration");
+    By cvvInput = By.id("cc-cvv");
+    By continueToCheckoutBtn = By.xpath("//button[contains(text(), 'Continue to checkout')]");
+
+    public BasketPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public BasketPage fillBillingAddress(String fName, String lName, String email, String address, String address2, String country, String city, String zip) {
+        Utility.sendKey(firstNameInput, fName, driver);
+        Utility.sendKey(lastNameInput, lName, driver);
+        Utility.sendKey(emailInput, email, driver);
+        Utility.sendKey(addressInput, address, driver);
+        Utility.sendKey(address2Input, address2, driver);
+        Utility.selectFromDropdown(countryDropdown, country, driver);
+        Utility.selectFromDropdown(cityDropdown, city, driver);
+        Utility.sendKey(zipInput, zip, driver);
+        return this;
+    }
+
+    public BasketPage fillPaymentDetails(String nameOnCard, String cardNumber, String expiration, String cvv) {
+        Utility.sendKey(nameOnCardInput, nameOnCard, driver);
+        Utility.sendKey(creditCardNumberInput, cardNumber, driver);
+        Utility.sendKey(expirationInput, expiration, driver);
+        Utility.sendKey(cvvInput, cvv, driver);
+        return this;
+    }
+
+    public void clickOnContinueToCheckout() {
+        Utility.click(continueToCheckoutBtn, driver);
+    }
+}
